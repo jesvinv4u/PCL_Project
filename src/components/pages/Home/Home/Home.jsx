b0b96666d274786aa6f3afb7feec2ef83386a993
sahalmahin <samirsatter0142@gmail.com>
2024-01-15 23:55:39 +0600

    created dynamic section title

--- src/components/pages/Home/Home/Home.jsx
+++ src/components/pages/Home/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from "../Banner/Banner";
+import MedicalCamp from "../MedicalCamp/MedicalCamp";
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <MedicalCamp></MedicalCamp>
         </div>
     );
 };
--- src/components/pages/Home/MedicalCamp/MedicalCamp.jsx
+++ src/components/pages/Home/MedicalCamp/MedicalCamp.jsx
@@ -1,7 +1,18 @@
+import { useEffect, useState } from "react";
+
 const MedicalCamp = () => {
+
+    const [camp, setCamp] = useState([]);
+
+    useEffect(() => {
+        fetch('camp.json')
+            .then(res => res.json())
+            .then(data => setCamp(data))
+    }, [])
+
     return (
         <div>
-            
+            <h3 className="text-9xl">Number of camp: {camp.length}</h3>
         </div>
     );
 };
